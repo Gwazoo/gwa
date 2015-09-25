6b9da8fe8b9e83fde6c7a7f05cfd235ca54444fe
fred <fredJ@gwazoo.com>
2015-09-25 17:04:20 -0600

    server.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,33 @@
 'use strict';
 angular.module('gwazoo', ['ngRoute', 'gwazoo.controllers', 'gwazoo.services'])
 
-.config(['$routeProvider', '$locationProvider',
-	function($routeProvider, $locationProvider) {
-		$routeProvider
-			.when('/', {
-				templateUrl: './templates/home.html',
-				controller: 'HomeCtrl'
-			})
-			.when('/sign-up', {
-				templateUrl: './templates/signup.html',
-				controller: 'SignupCtrl'
-			})
-			.when('/account', {
-				templateUrl: './templates/dashboard.html',
-				controller: 'DashboardCtrl'
-			})
-			.when('/category/:slug', {
-				templateUrl: './templates/categoryProducts.html',
-				controller: 'CategoryCtrl'
-			})
-			.when('/product/:sku', {
-				templateUrl: './templates/productDetails.html',
-				controller: 'ProductCtrl'
-			})
-			.otherwise({
-				redirectTo: '/'
-			});
-		$locationProvider.html5Mode(true);
-}]);
+.config(function($routeProvider, $locationProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: './templates/home.html',
+			controller: 'HomeCtrl'
+		})
+		.when('/sign-up', {
+			templateUrl: './templates/signup.html',
+			controller: 'SignupCtrl'
+		})
+		.when('/account', {
+			templateUrl: './templates/dashboard.html',
+			controller: 'DashboardCtrl'
+		})
+		.when('/category/:slug', {
+			templateUrl: './templates/categoryProducts.html',
+			controller: 'CategoryCtrl'
+		})
+		.when('/product/:sku', {
+			templateUrl: './templates/productDetails.html',
+			controller: 'ProductCtrl'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+	$locationProvider.html5Mode({
+		enabled: true,
+		requireBase: false
+	});
+});
--- server.js
+++ server.js
@@ -1,1 +1,39 @@
 'use strict';
+var Express = require('express'),
+	Session = require('express-session'),
+	BodyParser = require('body-parser'),
+	Passport = require('passport'),
+	LocalStrategy = require('passport-local').Strategy,
+	// Bcrypt = require('bcrypt'),
+	Connect = require('connect'),
+	Rewrite = require('express-rewrite'),
+	Q = require('q');//,
+	// R = require('rethink');
+
+// var userControl = require('./server/controls/userControl'),
+// 	productControl = require('./server/controls/productControl'),
+// 	categoryControl = require('./server/controls/categoryControl');
+
+var app = Express();
+
+
+// MIDDLEWARE
+app.use(Rewrite);
+// app.use(server.router);
+app.use('/', Express.static(__dirname + '/public'));
+
+app.get('/#/*', Rewrite.rewrite('/*'));
+
+// app.use(BodyParser.json());
+// app.use(Session({secret: 'gwazooTeam', saveUninitialized: true, resave: true}));
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+
+// AUTHENTICATION
+
+
+// ENDPOINTS
+
+
+// CONNECTIONS
