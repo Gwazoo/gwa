b528adc54a815c9664ec57937740d1d9251e8f09
fred <fredJ@gwazoo.com>
2015-10-08 08:24:15 -0600

    setting up ng-flow

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('gwazoo', ['gwazoo.controllers', 'gwazoo.services', 'ngRoute', /*'ngMaterial',*/ 'LocalStorageModule', 'flow'])
+angular.module('gwazoo', ['gwazoo.controllers', 'gwazoo.services', 'ngRoute', /*'ngMaterial',*/ 'LocalStorageModule', 'flow', /*'ui.bootstrap'*/])
 
 .config(function($routeProvider, $locationProvider) {
 	$routeProvider
