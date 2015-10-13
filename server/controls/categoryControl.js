91387d8eaf7cde64f28037c7cb1093dae7697f8a
Austin Welch <austinw@gwazoo.com>
2015-10-13 17:38:25 -0600

    Updated controls and models to remove some console logs

--- server/controls/categoryControl.js
+++ server/controls/categoryControl.js
@@ -13,14 +13,12 @@ module.exports = {
 	*/
 	getAll : function (req, res) {   
 		category.getAll(function (err, data) {
-			console.log("Data:", data);	
 			res.json(data);
 		});
 	},
 	getProducts : function (req, res) {
 		category.getProducts(req.params.id)
 		.then(function(result){
-			console.log("Data:", result);
 			res.json(result);
 		}, function (err) {
 			res.status(500).json({
