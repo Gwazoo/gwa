4b945ed8feac44823cf66a2f8b5a67a0ced320a0
fredrickJones <fredrick.w.jones@gmail.com>
2015-10-22 17:31:12 -0600

    working on the product details page

--- public/js/controllers/ProductCtrl.js
+++ public/js/controllers/ProductCtrl.js
@@ -1,54 +1,126 @@
 'use strict';
 angular.module('gwazoo.controllers')
 
-.controller('ProductCtrl', ['$scope', '$rootScope', 'breadcrumbs', 'Products', function($scope, $rootScope, breadcrumbs, Products) {
-    $scope.details = {
-        images: [
-            {
-                imageSmall: 'img/preview-small-img1.png',
-                imageLarge: 'img/preview-large-img1.jpg'
-            },
-            {
-                imageSmall: 'img/preview-small-img2.png',
-                imageLarge: 'img/preview-large-img2.jpg'
-            },
-            {
-                imageSmall: 'img/preview-small-img3.png',
-                imageLarge: 'img/preview-large-img3.jpg'
-            },
-            {
-                imageSmall: 'img/preview-small-img4.png',
-                imageLarge: 'img/preview-large-img4.jpg'
-            }
-        ],
-        name: 'Whirlpool duet steam front load washer',
-        description: 'Keep clothes looking newer longer with the Whirlpool 4.3 cu. ft. front load washer. Using adaptive wash actions and a concentrated solution of He detergent the Cold Wash cycle gently lifts stains and soils from fabrics to deliver the best care for your clothes. Precision Dispense takes the guesswork out of adding detergent bleach and other additives by releasing them at the correct time during the cycle for optimal fabric care. Detergent mixes with water to penetrate fabrics and virtually eliminate pretreating. Tap Touch controls allow direct access to cycles and options like the TumbleFresh option. It keeps clothes fresh and reduces wrinkles by tumbling clothes after the cycle\'s end for up to 6 hours',
-        price: '965.50',
-        retailPrice: '1846.23',
-        rate: '4.8'
+.controller('ProductCtrl', ['$scope', '$rootScope', 'breadcrumbs', 'Products', 'Cookies', function($scope, $rootScope, breadcrumbs, Products, Cookies) {
+    // $scope.rate = 7;
+    // $scope.max = 5;
+    // $scope.isReadonly = false;
+
+    // $scope.hoveringOver = function(value) {
+    //     $scope.overStar = value;
+    //     $scope.percent = 100 * (value / $scope.max);
+    // };
+    
+    $scope.add = function (productId) {
+        Cookies.add(productId);
+        $scope.cartCount = Cookies.getCartCount();
     };
-}])
 
-// .directive('ngElevateZoom', function() {
-//     return {
-//         restrict: 'A',
-//         link: function(scope, element, attrs) {
-//             console.log("Linking");
-
-//             //Will watch for changes on the attribute
-//             attrs.$observe('zoomImage',function(){
-//                 linkElevateZoom();
-//             });
-      
-//             function linkElevateZoom() {
-//                 //Check if its not empty
-//                 if (!attrs.zoomImage) return;
-//                 element.attr('data-zoom-image',attrs.zoomImage);
-//                 $(element).elevateZoom();
-//             }
-//             linkElevateZoom();
-//         }
-//     };
-// });
+    $scope.tester = {  
+       "optionSet":{  
+          "name":"New Shirt",
+          "options":[  
+             {  
+                "name":"Color",
+                "variations":[  
+                   "Blue",
+                   "Green",
+                   "Red"
+                ]
+             },
+             {  
+                "name":"Size",
+                "variations":[  
+                   "Small",
+                   "Medium",
+                   "Large"
+                ]
+             }
+          ]
+       },
+       "additionalInfo":[  
+          {  
+             "name":"Cool?",
+             "value":"Yes!"
+          }
+       ],
+       "categories":[  
+          {  
+             "id":"8332f4ef-e262-43ea-86be-dd17afc1ffbe",
+             "name":"Clothing, Shoes & Jewelry"
+          }
+       ],
+       "created":"2015-10-22T16:52:30.515Z",
+       "description":"A shirt that you wear",
+       "id":"18dbebcd-8c5a-476b-b84b-f15232235337",
+       "images":[  
+          {  
+             "large":{  
+                "url":"http://www.jamesbondlifestyle.com/sites/default/files/styles/fancybox_popup/public/images/product/cl014-sunspel-grey-shirt.jpg",
+                "isPrimary":false
+             },
+             "small":{  
+                "isPrimary":true,
+                "url":"http://www.jamesbondlifestyle.com/sites/default/files/styles/fancybox_popup/public/images/product/cl014-sunspel-grey-shirt.jpg"
+             }
+          }
+       ],
+       "isActive":true,
+       "items":[  
+          {  
+             "additionalInfo":[  
+                {  
+                   "name":"Cool?",
+                   "value":"Very!"
+                }
+             ],
+             "created":"2015-10-22T17:11:46.448Z",
+             "description":"A blue shirt that you wear",
+             "id":"4b4d8ddb-d836-44fd-9d06-dc55b52f7327",
+             "images":[  
+                {  
+                   "largeImage":{  
+                      "url":"http://tattoolicious.com/assets/uploads/images/T-Shirt.jpg",
+                      "isPrimary":false
+                   },
+                   "smallImage":{  
+                      "isPrimary":true,
+                      "url":"http://tattoolicious.com/assets/uploads/images/T-Shirt.jpg"
+                   }
+                }
+             ],
+             "isActive":true,
+             "maxQuantity":10,
+             "minQuantity":1,
+             "modified":"2015-10-22T17:11:46.448Z",
+             "name":"Blue Shirt",
+             "options":{  
+                "color":"Blue",
+                "size":"Medium"
+             },
+             "parentId":"18dbebcd-8c5a-476b-b84b-f15232235337",
+             "shortDescription":"A blue shirt",
+             "sku":"1Z2X3CB",
+             "stockQuantity":20,
+             "vendor":"anewtest"
+          }
+       ],
+       "maxQuantity":10,
+       "minQuantity":1,
+       "modified":"2015-10-22T16:52:30.515Z",
+       "msrp":"15.00",
+       "name":"Shirt",
+       "optionSetId":"9c9f100f-b775-4a8f-9ac9-611581e1da02",
+       "retailPrice":"10.00",
+       "shippingPrice":"2.50",
+       "shippingType":"flat",
+       "shortDescription":"A shirt",
+       "sku":"1Z2X3C",
+       "stockQuantity":100,
+       "supplyPrice":"5.00",
+       "vendor":"anewtest"
+    };
+    console.log($scope.tester);
+}]);
 
 
---
+++ public/js/services/Cart.js
@@ -0,0 +1,8 @@
+'use strict';
+angular.module('gwazoo.services')
+
+.service('Cart', function ($q, $http, $rootScope) {
+    this.addToCart = function (item) {
+        
+    };
+});
