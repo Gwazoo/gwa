a7377e81059eaf14ba07654eff4f42876de5eb80
fredrickJones <fredrick.w.jones@gmail.com>
2015-10-28 14:18:34 -0600

    working on fixes for customer experience

--- public/js/controllers/CartCtrl.js
+++ public/js/controllers/CartCtrl.js
@@ -1,7 +1,19 @@
 'use strict';
 angular.module('gwazoo.controllers')
 
-.controller('CartCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+.controller('CartCtrl', ['$scope', '$rootScope', 'Cookies', function($scope, $rootScope, Cookies) {
+
+    function getCart() {
+        var cartProducts= Cookies.getCart();
+        console.log(cartProducts);
+        for (var i = 0; i < cartProducts.length; i++) {
+            var cartItem = cartProducts[i];
+        };
+        console.log(cartItem);
+        $scope.cart = cartItem;
+        console.log($scope.cart);
+    };
+    getCart();
 
 }]);
 
