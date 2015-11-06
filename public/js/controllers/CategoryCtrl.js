90617e95a7502f37faef42d2af273c537fd3ba3b
fredrickJones <fredrick.w.jones@gmail.com>
2015-11-06 10:34:13 -0700

    changes to category product page

--- public/js/controllers/CategoryCtrl.js
+++ public/js/controllers/CategoryCtrl.js
@@ -2,14 +2,20 @@
 angular.module('gwazoo.controllers')
 
 .controller('CategoryCtrl', ['$scope', '$rootScope', '$stateParams', 'Products', function($scope, $rootScope, $stateParams, Products) {
-    
-    console.log($stateParams);
+    // $scope.rate = 4.2;
+    // $scope.max = 5;
+    // $scope.isReadonly = true;
+    $scope.itemOptions = {options:[]};
+    $scope.options = [];
+    $scope.item = {};
+
+    // console.log($stateParams);
     Products.getCategoryProducts($stateParams.id)
     .then(function (result) {
-        // console.log(result);
+        console.log(result);
         $scope.category = result.name;
         $scope.products = result.products;
-        $scope.image = result.image;
+        console.log(result.products[0].images[0].large.url);
     });
 }]);
 
